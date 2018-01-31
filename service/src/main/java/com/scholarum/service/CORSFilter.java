3c8fd422f412da880743bd6b91030bc880f0078f
Vikash Kumar <vikash.kumar@talentica.com>
2018-01-31 11:13:00 +0530

    Allowing CORS support

---
+++ service/src/main/java/com/scholarum/service/CORSFilter.java
@@ -0,0 +1,37 @@
+package com.scholarum.service;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CORSFilter implements Filter {
+	@Override
+	public void destroy() {
+		// Nothing to do
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletResponse res = (HttpServletResponse) response;
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader("Access-Control-Allow-Methods", "*");
+		res.addHeader("Access-Control-Allow-Headers",
+				"Content-Type,Accept,X-Requested-With,accessKey,vaultSignature,signature,data");
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// Nothing to do
+	}
+}
